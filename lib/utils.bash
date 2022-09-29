de3559b2a8debc82de0fcb191b2c9d16e92ea5e6
Tommy Bø <tommy.andre.bo@entur.org>
2022-09-29 13:58:02 +0200

    fix: updated version test to remove commit hash

--- lib/utils.bash
+++ lib/utils.bash
@@ -57,7 +57,7 @@ install_version() {
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
     local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+    tool_cmd="$(echo "$TOOL_TEST" | cut -d'+' -f1)"
     test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
     echo "$TOOL_NAME $version installation was successful!"
